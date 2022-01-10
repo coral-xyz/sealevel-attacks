5069145267ff453dcee2f857e59112ea61a5b51f
Armani Ferrante <armaniferrante@gmail.com>
2022-01-10 18:19:48 -0500

    Init

---
+++ tests/raw.ts
@@ -0,0 +1,16 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { Raw } from '../target/types/raw';
+
+describe('raw', () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.Raw as Program<Raw>;
+
+  it('Is initialized!', async () => {
+    // Add your test here.
+    const tx = await program.rpc.initialize({});
+    console.log("Your transaction signature", tx);
+  });
+});
