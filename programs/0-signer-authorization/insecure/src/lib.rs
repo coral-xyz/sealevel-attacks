817bfb0b560fd52cd61663164554dc86a7f37384
youarj <yourarj@gmail.com>
2022-05-01 16:57:45 +0530

    fix(bump-seed): remove unnecessary changes

--- programs/0-signer-authorization/insecure/src/lib.rs
+++ programs/0-signer-authorization/insecure/src/lib.rs
@@ -1,20 +1,11 @@
 use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
-/// Objective: Authenticating Signer of transaction
-///
-/// Topics: [AccountInfo](https://docs.rs/anchor-lang/0.24.2/anchor_lang/prelude/struct.AccountInfo.html), [Signer](https://docs.rs/anchor-lang/0.24.2/anchor_lang/accounts/signer/struct.Signer.html)
-///
-/// The following program example accepts
-/// arbitrary solana account hence not safe.
-/// and log_message method is also not performing any
-/// safety check
 #[program]
 pub mod signer_authorization_insecure {
     use super::*;
 
     pub fn log_message(ctx: Context<LogMessage>) -> ProgramResult {
-        // no account checks performed here
         msg!("GM {}", ctx.accounts.authority.key().to_string());
         Ok(())
     }
@@ -22,7 +13,5 @@ pub mod signer_authorization_insecure {
 
 #[derive(Accounts)]
 pub struct LogMessage<'info> {
-    /// arbitrary solana account\
-    /// read more about [AccountInfo](https://docs.rs/anchor-lang/0.24.2/anchor_lang/prelude/struct.AccountInfo.html)
     authority: AccountInfo<'info>,
 }
--- programs/0-signer-authorization/recommended/src/lib.rs
+++ programs/0-signer-authorization/recommended/src/lib.rs
@@ -2,16 +2,11 @@ use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
-/// Utilizes anchor's Signer to authenticate
-/// the transaction signer
-///
-/// [Signer](https://docs.rs/anchor-lang/0.24.2/anchor_lang/accounts/signer/struct.Signer.html)
 #[program]
 pub mod signer_authorization_recommended {
     use super::*;
 
     pub fn log_message(ctx: Context<LogMessage>) -> ProgramResult {
-        // Signer verifies authenticity no need of explicit checks here
         msg!("GM {}", ctx.accounts.authority.key().to_string());
         Ok(())
     }
@@ -19,6 +14,5 @@ pub mod signer_authorization_recommended {
 
 #[derive(Accounts)]
 pub struct LogMessage<'info> {
-    /// signer performs Signer.info.is_signer == true check
     authority: Signer<'info>,
 }
