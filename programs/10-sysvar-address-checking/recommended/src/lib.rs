4b0ba68af50c071962bccb199e0969988b823893
anoushk1234 <anoushka1828@gmail.com>
2022-07-14 12:13:53 +0530

    manual check

--- programs/10-sysvar-address-checking/recommended/src/lib.rs
+++ programs/10-sysvar-address-checking/recommended/src/lib.rs
@@ -1,13 +1,12 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::sysvar;
+
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod secure {
+pub mod recommended {
     use super::*;
 
     pub fn check_sysvar_address(ctx: Context<CheckSysvarAddress>) -> Result<()> {
-        require_eq!(ctx.accounts.rent.key(), sysvar::rent::ID);
         msg!("Rent Key -> {}", ctx.accounts.rent.key().to_string());
         Ok(())
     }
@@ -15,5 +14,5 @@ pub mod secure {
 
 #[derive(Accounts)]
 pub struct CheckSysvarAddress<'info> {
-    rent: AccountInfo<'info>,
+    rent: Sysvar<'info, Rent>,
 }
